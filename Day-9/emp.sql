13d43bbc0a65d4222d035820f1c3b39c39494bb0
Suseela Selvakumar <2326ka52@kgcas.com>
2025-06-17 12:44:42 +0530

    Update emp.sql

--- Day-9/emp.sql
+++ Day-9/emp.sql
@@ -377,3 +377,184 @@ mysql> select *
 |  7934 | MILLER | CLERK     | 7782 | 1982-01-23 | 1300.00 |    NULL |     10 |
 +-------+--------+-----------+------+------------+---------+---------+--------+
 9 rows in set (0.00 sec)
+
+mysql> select *
+    -> from emp
+    -> where hiredate<'1981-12-31'
+    -> AND hiredate<'1987-01-01';
++-------+--------+-----------+------+------------+---------+---------+--------+
+| empno | ename  | job       | mgr  | hiredate   | sal     | comm    | deptno |
++-------+--------+-----------+------+------------+---------+---------+--------+
+|  7369 | SMITH  | CLERK     | 7902 | 1980-12-17 |  800.00 |    NULL |     20 |
+|  7499 | ALLEN  | SALESMAN  | 7698 | 1981-02-20 | 1600.00 |  300.00 |     30 |
+|  7521 | WARD   | SALESMAN  | 7698 | 1981-02-22 | 1250.00 |  500.00 |     30 |
+|  7566 | JONES  | MANAGER   | 7839 | 1981-04-02 | 2975.00 |    NULL |     20 |
+|  7654 | MARTIN | SALESMAN  | 7698 | 1981-09-28 | 1250.00 | 1400.00 |     30 |
+|  7698 | BLAKE  | MANAGER   | 7839 | 1981-05-01 | 2850.00 |    NULL |     30 |
+|  7782 | CLARK  | MANAGER   | 7839 | 1981-06-09 | 2450.00 |    NULL |     10 |
+|  7839 | KING   | PRESIDENT | NULL | 1981-11-17 | 5000.00 |    NULL |     10 |
+|  7844 | TURNER | SALESMAN  | 7698 | 1981-09-08 | 1500.00 |    0.00 |     30 |
+|  7900 | JAMES  | CLERK     | 7698 | 1981-12-03 |  950.00 |    NULL |     30 |
+|  7902 | FORD   | ANALYST   | 7566 | 1981-12-03 | 3000.00 |    NULL |     20 |
++-------+--------+-----------+------+------------+---------+---------+--------+
+11 rows in set (0.00 sec)
+
+mysql> select ename
+    -> from emp
+    -> where hiredate<'1981-12-31'
+    -> AND deptno in (10, 30);
++--------+
+| ename  |
++--------+
+| ALLEN  |
+| WARD   |
+| MARTIN |
+| BLAKE  |
+| CLARK  |
+| KING   |
+| TURNER |
+| JAMES  |
++--------+
+8 rows in set (0.00 sec)
+
+mysql> select *,sal*12
+    -> from emp
+    -> where job NOT IN ('analyst', 'manager');
++-------+--------+-----------+------+------------+---------+---------+--------+----------+
+| empno | ename  | job       | mgr  | hiredate   | sal     | comm    | deptno | sal*12   |
++-------+--------+-----------+------+------------+---------+---------+--------+----------+
+|  7369 | SMITH  | CLERK     | 7902 | 1980-12-17 |  800.00 |    NULL |     20 |  9600.00 |
+|  7499 | ALLEN  | SALESMAN  | 7698 | 1981-02-20 | 1600.00 |  300.00 |     30 | 19200.00 |
+|  7521 | WARD   | SALESMAN  | 7698 | 1981-02-22 | 1250.00 |  500.00 |     30 | 15000.00 |
+|  7654 | MARTIN | SALESMAN  | 7698 | 1981-09-28 | 1250.00 | 1400.00 |     30 | 15000.00 |
+|  7839 | KING   | PRESIDENT | NULL | 1981-11-17 | 5000.00 |    NULL |     10 | 60000.00 |
+|  7844 | TURNER | SALESMAN  | 7698 | 1981-09-08 | 1500.00 |    0.00 |     30 | 18000.00 |
+|  7876 | ADAMS  | CLERK     | 7788 | 1983-01-12 | 1100.00 |    NULL |     20 | 13200.00 |
+|  7900 | JAMES  | CLERK     | 7698 | 1981-12-03 |  950.00 |    NULL |     30 | 11400.00 |
+|  7934 | MILLER | CLERK     | 7782 | 1982-01-23 | 1300.00 |    NULL |     10 | 15600.00 |
++-------+--------+-----------+------+------------+---------+---------+--------+----------+
+9 rows in set (0.00 sec)
+
+mysql> select count(*)
+    -> from emp
+    -> where sal<2000 and deptno=30;
++----------+
+| count(*) |
++----------+
+|        5 |
++----------+
+1 row in set (0.00 sec)
+
+mysql> select deptno , sal
+    -> from emp
+    -> where deptno=10;
++--------+---------+
+| deptno | sal     |
++--------+---------+
+|     10 | 2450.00 |
+|     10 | 5000.00 |
+|     10 | 1300.00 |
++--------+---------+
+3 rows in set (0.00 sec)
+
+mysql> select sum(sal)
+    -> from emp
+    -> where job='clerl';
++----------+
+| sum(sal) |
++----------+
+|     NULL |
++----------+
+1 row in set (0.00 sec)
+
+mysql> select sum(sal)
+    -> from emp
+    -> where job='clerk';
++----------+
+| sum(sal) |
++----------+
+|  4150.00 |
++----------+
+1 row in set (0.00 sec)
+
+mysql> select sum(sal),ename
+    -> from emp
+    -> where job='clerk'
+    -> group by ename;
++----------+--------+
+| sum(sal) | ename  |
++----------+--------+
+|   800.00 | SMITH  |
+|  1100.00 | ADAMS  |
+|   950.00 | JAMES  |
+|  1300.00 | MILLER |
++----------+--------+
+4 rows in set (0.00 sec)
+
+mysql> select * from emp
+    -> ;
++-------+--------+-----------+------+------------+---------+---------+--------+
+| empno | ename  | job       | mgr  | hiredate   | sal     | comm    | deptno |
++-------+--------+-----------+------+------------+---------+---------+--------+
+|  7369 | SMITH  | CLERK     | 7902 | 1980-12-17 |  800.00 |    NULL |     20 |
+|  7499 | ALLEN  | SALESMAN  | 7698 | 1981-02-20 | 1600.00 |  300.00 |     30 |
+|  7521 | WARD   | SALESMAN  | 7698 | 1981-02-22 | 1250.00 |  500.00 |     30 |
+|  7566 | JONES  | MANAGER   | 7839 | 1981-04-02 | 2975.00 |    NULL |     20 |
+|  7654 | MARTIN | SALESMAN  | 7698 | 1981-09-28 | 1250.00 | 1400.00 |     30 |
+|  7698 | BLAKE  | MANAGER   | 7839 | 1981-05-01 | 2850.00 |    NULL |     30 |
+|  7782 | CLARK  | MANAGER   | 7839 | 1981-06-09 | 2450.00 |    NULL |     10 |
+|  7788 | SCOTT  | ANALYST   | 7566 | 1982-12-09 | 3000.00 |    NULL |     20 |
+|  7839 | KING   | PRESIDENT | NULL | 1981-11-17 | 5000.00 |    NULL |     10 |
+|  7844 | TURNER | SALESMAN  | 7698 | 1981-09-08 | 1500.00 |    0.00 |     30 |
+|  7876 | ADAMS  | CLERK     | 7788 | 1983-01-12 | 1100.00 |    NULL |     20 |
+|  7900 | JAMES  | CLERK     | 7698 | 1981-12-03 |  950.00 |    NULL |     30 |
+|  7902 | FORD   | ANALYST   | 7566 | 1981-12-03 | 3000.00 |    NULL |     20 |
+|  7934 | MILLER | CLERK     | 7782 | 1982-01-23 | 1300.00 |    NULL |     10 |
++-------+--------+-----------+------+------------+---------+---------+--------+
+14 rows in set (0.00 sec)
+
+mysql> select avg(sal)
+    -> from emp;
++-------------+
+| avg(sal)    |
++-------------+
+| 2073.214286 |
++-------------+
+1 row in set (0.00 sec)
+
+mysql> select sum(sal),job
+    -> from emp
+    -> group by job;
++----------+-----------+
+| sum(sal) | job       |
++----------+-----------+
+|  4150.00 | CLERK     |
+|  5600.00 | SALESMAN  |
+|  8275.00 | MANAGER   |
+|  6000.00 | ANALYST   |
+|  5000.00 | PRESIDENT |
++----------+-----------+
+5 rows in set (0.00 sec)
+
+mysql> select
+    -> AVG(sal) AS avg_salary,
+    -> SUM(sal) AS sum_salary,
+    -> COUNT(*) AS num_emp,
+    -> MAX(sal) AS max_salary
+    -> from emp
+    -> where job ='president';
++-------------+------------+---------+------------+
+| avg_salary  | sum_salary | num_emp | max_salary |
++-------------+------------+---------+------------+
+| 5000.000000 |    5000.00 |       1 |    5000.00 |
++-------------+------------+---------+------------+
+1 row in set (0.00 sec)
+
+mysql> select count(*)
+    -> from emp;
++----------+
+| count(*) |
++----------+
+|       14 |
++----------+
+1 row in set (0.00 sec)
