7f35c35947b994628512ed6a33bb48f5bcb7ca0d
Suseela Selvakumar <2326ka52@kgcas.com>
2025-06-19 15:20:51 +0530

    Update Day-11.sql

--- SQL Task/Day-11.sql
+++ SQL Task/Day-11.sql
@@ -113,6 +113,62 @@ mysql> select * from marks;
 +----------+
 7 rows in set (0.00 sec)
 
+mysql> select *
+    -> from student cross join marks;
++--------+-------+-----------+----------+
+| sname  | sroll | Subject   | StudMark |
++--------+-------+-----------+----------+
+| Joonie |  1998 | Chemistry |       99 |
+| Hope   |  1993 | History   |       99 |
+| Moonie |  1992 | Physics   |       99 |
+| Yoongi |  1994 | Arts      |       99 |
+| Mochi  |  1996 | Dance     |       99 |
+| Kookie |  1997 | Sports    |       99 |
+| Taeshi |  1995 | Maths     |       99 |
+| Joonie |  1998 | Chemistry |       95 |
+| Hope   |  1993 | History   |       95 |
+| Moonie |  1992 | Physics   |       95 |
+| Yoongi |  1994 | Arts      |       95 |
+| Mochi  |  1996 | Dance     |       95 |
+| Kookie |  1997 | Sports    |       95 |
+| Taeshi |  1995 | Maths     |       95 |
+| Joonie |  1998 | Chemistry |       92 |
+| Hope   |  1993 | History   |       92 |
+| Moonie |  1992 | Physics   |       92 |
+| Yoongi |  1994 | Arts      |       92 |
+| Mochi  |  1996 | Dance     |       92 |
+| Kookie |  1997 | Sports    |       92 |
+| Taeshi |  1995 | Maths     |       92 |
+| Joonie |  1998 | Chemistry |       89 |
+| Hope   |  1993 | History   |       89 |
+| Moonie |  1992 | Physics   |       89 |
+| Yoongi |  1994 | Arts      |       89 |
+| Mochi  |  1996 | Dance     |       89 |
+| Kookie |  1997 | Sports    |       89 |
+| Taeshi |  1995 | Maths     |       89 |
+| Joonie |  1998 | Chemistry |       85 |
+| Hope   |  1993 | History   |       85 |
+| Moonie |  1992 | Physics   |       85 |
+| Yoongi |  1994 | Arts      |       85 |
+| Mochi  |  1996 | Dance     |       85 |
+| Kookie |  1997 | Sports    |       85 |
+| Taeshi |  1995 | Maths     |       85 |
+| Joonie |  1998 | Chemistry |       83 |
+| Hope   |  1993 | History   |       83 |
+| Moonie |  1992 | Physics   |       83 |
+| Yoongi |  1994 | Arts      |       83 |
+| Mochi  |  1996 | Dance     |       83 |
+| Kookie |  1997 | Sports    |       83 |
+| Taeshi |  1995 | Maths     |       83 |
+| Joonie |  1998 | Chemistry |       81 |
+| Hope   |  1993 | History   |       81 |
+| Moonie |  1992 | Physics   |       81 |
+| Yoongi |  1994 | Arts      |       81 |
+| Mochi  |  1996 | Dance     |       81 |
+| Kookie |  1997 | Sports    |       81 |
+| Taeshi |  1995 | Maths     |       81 |
++--------+-------+-----------+----------+
+49 rows in set (0.00 sec)
 
 2. Sub-query to find top performer
 mysql> update student set sroll=1998
