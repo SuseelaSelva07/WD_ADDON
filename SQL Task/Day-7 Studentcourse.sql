6bad2109b53cc0a8b6ced3bc25b735550f8b7642
Suseela Selvakumar <2326ka52@kgcas.com>
2025-06-19 14:21:24 +0530

    Update Day-7 Studentcourse.sql

--- SQL Task/Day-7 Studentcourse.sql
+++ SQL Task/Day-7 Studentcourse.sql
@@ -1,3 +1,5 @@
+1.Create student and course tables
+
 mysql> show databases;
 +--------------------+
 | Database           |
@@ -29,6 +31,8 @@ mysql> create table Student (
     -> );
 Query OK, 0 rows affected (0.03 sec)
 
+2. Insert 5 records in each
+    
 mysql> insert into Student values('Taeshi',1995,'taeshi@gmail.com',30121995,'Maths',25000);
 Query OK, 1 row affected (0.00 sec)
 
@@ -58,6 +62,8 @@ Query OK, 1 row affected (0.00 sec)
 mysql> insert into Student values('Joonie',1995,'joonie@gmail.com',12091994,'Chemistry',19500);
 Query OK, 1 row affected (0.00 sec)
 
+3.Update a course fee    
+    
 mysql> select * from student;
 +--------+-------+------------------+----------+-----------+----------+
 | sname  | sroll | sgmail           | phnum    | course    | courfees |
@@ -72,32 +78,3 @@ mysql> select * from student;
 +--------+-------+------------------+----------+-----------+----------+
 7 rows in set (0.00 sec)
 
-mysql> select * from student ORDER BY courfees ASC;
-+--------+-------+------------------+----------+-----------+----------+
-| sname  | sroll | sgmail           | phnum    | course    | courfees |
-+--------+-------+------------------+----------+-----------+----------+
-| Joonie |  1995 | joonie@gmail.com | 12091994 | Chemistry |    19500 |
-| Hope   |  1994 | hope@gmail.com   | 18021994 | History   |    19800 |
-| Yoongi |  1994 | yoongi@gmail.com | 88031994 | Arts      |    20000 |
-| Moonie |  1992 | moomie@gmail.com | 44121992 | Physics   |    20500 |
-| Mochi  |  1996 | mochi@gmail.com  | 13101995 | Dance     |    21500 |
-| Kookie |  1997 | kookie@gmail.com | 11091997 | Sports    |    22000 |
-| Taeshi |  1995 | taeshi@gmail.com | 30121995 | Maths     |    25000 |
-+--------+-------+------------------+----------+-----------+----------+
-7 rows in set (0.00 sec)
-
-mysql> select courfees
-    -> from student
-    -> ORDER BY courfees ASC;
-+----------+
-| courfees |
-+----------+
-|    19500 |
-|    19800 |
-|    20000 |
-|    20500 |
-|    21500 |
-|    22000 |
-|    25000 |
-+----------+
-7 rows in set (0.00 sec)
