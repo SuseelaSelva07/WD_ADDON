fd71b34b5e1488ac503ff0155effe561960c0926
Suseela Selvakumar <2326ka52@kgcas.com>
2025-06-29 19:17:26 +0530

    Create Task-1 Evennumber.java

---
+++ Task/Day-1 Task/Task-1 Evennumber.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Evennumber {
+    public static void main(String[] args) {
+       System.out.println("Enter the starting number:");
+       Scanner in=new Scanner(System.in);
+       int a=in.nextInt();
+     System.out.println("Enter the last number:");
+     int b=in.nextInt();
+     for(int i=a; i<b; i++)
+     if(i%2==0)
+     {
+     System.out.println(i);
+     }
+    }
+}
