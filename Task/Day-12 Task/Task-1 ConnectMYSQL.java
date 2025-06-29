b66a2a384cef9bb2a283212d75f6c1d240216b5c
Suseela Selvakumar <2326ka52@kgcas.com>
2025-06-29 21:39:38 +0530

    Create Task-1 ConnectMYSQL.java

---
+++ Task/Day-12 Task/Task-1 ConnectMYSQL.java
@@ -0,0 +1,23 @@
+import java.sql.*;
+
+public class ConnectMYSQL
+{
+    public static void main(String[] args) 
+    {
+        String url = "jdbc:mysql://localhost:3306/testdb"; 
+        String user = "root"; 
+        String pass = "password"; 
+
+        try 
+        {
+            Connection con = DriverManager.getConnection(url, user, pass);
+            System.out.println("Connected to MySQL successfully!");
+            con.close();
+        } 
+        catch (Exception e) 
+        {
+            System.out.println("Connection failed!");
+            e.printStackTrace();
+        }
+    }
+}
