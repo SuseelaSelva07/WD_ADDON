2c816b18a4628f5436e1cb36607657cf7c9b5308
Suseela Selvakumar <2326ka52@kgcas.com>
2025-06-29 20:46:17 +0530

    Create Task-2 5_records.sql

---
+++ Task/Day-7 Task/Task-2 5_records.sql
@@ -0,0 +1,32 @@
+
+2. Insert 5 records in each
+    
+mysql> insert into Student values('Taeshi',1995,'taeshi@gmail.com',30121995,'Maths',25000);
+Query OK, 1 row affected (0.00 sec)
+
+mysql> select * from student;
++--------+-------+------------------+----------+--------+----------+
+| sname  | sroll | sgmail           | phnum    | course | courfees |
++--------+-------+------------------+----------+--------+----------+
+| Taeshi |  1995 | taeshi@gmail.com | 30121995 | Maths  |    25000 |
++--------+-------+------------------+----------+--------+----------+
+1 row in set (0.00 sec)
+
+mysql> insert into Student values('Kookie',1997,'kookie@gmail.com',11091997,'Sports',22000);
+Query OK, 1 row affected (0.00 sec)
+
+mysql> insert into Student values('Mochi',1996,'mochi@gmail.com',13101995,'Dance',21500);
+Query OK, 1 row affected (0.00 sec)
+
+mysql> insert into Student values('Yoongi',1994,'yoongi@gmail.com',88031994,'Arts',20000);
+Query OK, 1 row affected (0.00 sec)
+
+mysql> insert into Student values('Moonie',1992,'moomie@gmail.com',44121992,'Physics',20500);
+Query OK, 1 row affected (0.00 sec)
+
+mysql> insert into Student values('Hope',1994,'hope@gmail.com',18021994,'History',19800);
+Query OK, 1 row affected (0.00 sec)
+
+mysql> insert into Student values('Joonie',1995,'joonie@gmail.com',12091994,'Chemistry',19500);
+Query OK, 1 row affected (0.00 sec)
+
