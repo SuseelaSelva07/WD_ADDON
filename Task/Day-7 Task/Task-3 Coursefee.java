296e24f82e91c39e24f36e5acc4cb5799f33da94
Suseela Selvakumar <2326ka52@kgcas.com>
2025-06-29 20:47:41 +0530

    Create Task-3 Coursefee.java

---
+++ Task/Day-7 Task/Task-3 Coursefee.java
@@ -0,0 +1,15 @@
+3.Update a course fee    
+    
+mysql> select * from student;
++--------+-------+------------------+----------+-----------+----------+
+| sname  | sroll | sgmail           | phnum    | course    | courfees |
++--------+-------+------------------+----------+-----------+----------+
+| Taeshi |  1995 | taeshi@gmail.com | 30121995 | Maths     |    25000 |
+| Kookie |  1997 | kookie@gmail.com | 11091997 | Sports    |    22000 |
+| Mochi  |  1996 | mochi@gmail.com  | 13101995 | Dance     |    21500 |
+| Yoongi |  1994 | yoongi@gmail.com | 88031994 | Arts      |    20000 |
+| Moonie |  1992 | moomie@gmail.com | 44121992 | Physics   |    20500 |
+| Hope   |  1994 | hope@gmail.com   | 18021994 | History   |    19800 |
+| Joonie |  1995 | joonie@gmail.com | 12091994 | Chemistry |    19500 |
++--------+-------+------------------+----------+-----------+----------+
+7 rows in set (0.00 sec)
